6067c4a8e7a028a1e76ce88d470c3a3829cfbe32
Korosh Taebi <korosh_taebi@deichmann.com>
2022-08-20 00:37:05 +0200

    add button to insert current timestamp in table row

--- js/script.js
+++ js/script.js
@@ -1,2 +1,11 @@
-document.addEventListener("DOMContentLoaded",);
+document.addEventListener("DOMContentLoaded", function (event) {
+    document.querySelector("button").addEventListener("click", function (event) {
+          var tbodyRef = document.querySelector("table").getElementsByTagName('tbody')[0];
+          var newRow = tbodyRef.insertRow(0);
+          var cell1 = newRow.insertCell(0);
+          var cell2 = newRow.insertCell(1);
+          cell1.innerHTML = "NEW CELL1";
+          cell2.innerHTML = Date(Date.now());
+    });
+});
 
