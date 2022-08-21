e7b957cc510ef118bcf66469e8cc5d3150036735
Korosh Taebi <korosh_taebi@deichmann.com>
2022-08-21 22:22:23 +0200

    add Teams as objects. Make sure buyer is only one of the players

--- js/script.js
+++ js/script.js
@@ -1,8 +1,19 @@
 document.addEventListener("DOMContentLoaded", function (event) {
+    var Team1 = {"Player1": "A",
+                 "Player2": "B",
+                 "totalScore": 0,
+                 "no_purchases": 0,
+                 "no_shelems": 0};
+    var Team2 = {"Player1": "C",
+                 "Player2": "D",
+                 "totalScore": 0,
+                 "no_purchases": 0,
+                 "no_shelems": 0};
+
+    var playerNames = [Team1.Player1, Team1.Player2, Team2.Player1, Team2.Player2];
     const Mul = arr => arr.map(item => item * 5);
     const allowedPrices = Mul(Array.from({length:33},(v,k)=>k+1));
     var roundNumber = 0
-    //var currentBuyingPrice;
 
     document.querySelector("button").addEventListener("click", function (event) {
           var tbodyRef = document.querySelector("table").getElementsByTagName('tbody')[0];
@@ -14,7 +25,11 @@ document.addEventListener("DOMContentLoaded", function (event) {
           cell1.innerHTML = roundNumber;
           roundNumber += 1;
           cell2.innerHTML = Date(Date.now());
-          cell3.innerHTML = prompt("Who bought?");
+          do {
+              var currentBuyer = prompt("Who bought?");
+          }
+          while (!playerNames.includes(currentBuyer));
+          cell3.innerHTML = currentBuyer;
 
           do {
               var currentBuyingPrice = prompt("how much?");
